69c1466da7e06cf2da395173990e3c6202cf5118
Eryk Sandecki <eryk.sandecki@gmail.com>
2025-09-18 22:14:42 +0200

    XD: omit enum none from anchors

--- src/components/PageBuilder/components/ViewBox/components/TransformArea/TransformArea.tsx
+++ src/components/PageBuilder/components/ViewBox/components/TransformArea/TransformArea.tsx
@@ -62,46 +62,50 @@ const TransformArea: FC<TTransformAreaProps> = ({
       style={{ height, width }}
     >
       {/* RESIZE AREA */}
-      {enumToArray(AnchorResize).map((anchor) => (
-        <E2EDataAttribute
-          key={anchor as keyof typeof AnchorResize}
-          type={E2EAttribute.anchorResize}
-          value={kebabCase(anchor as string)}
-        >
-          <div
-            className={cx(
-              classNamesWithTheme.anchorResize.name,
-              classNamesWithTheme.anchorResize.modificators[
-                anchor as keyof typeof AnchorResize
-              ],
-            )}
-            onMouseDown={(event) =>
-              onMouseDownAnchorResize(anchor as AnchorResize, event)
-            }
-          />
-        </E2EDataAttribute>
-      ))}
+      {enumToArray(AnchorResize)
+        .filter((anchor) => anchor !== AnchorRotate.none)
+        .map((anchor) => (
+          <E2EDataAttribute
+            key={anchor as keyof typeof AnchorResize}
+            type={E2EAttribute.anchorResize}
+            value={kebabCase(anchor as string)}
+          >
+            <div
+              className={cx(
+                classNamesWithTheme.anchorResize.name,
+                classNamesWithTheme.anchorResize.modificators[
+                  anchor as keyof typeof AnchorResize
+                ],
+              )}
+              onMouseDown={(event) =>
+                onMouseDownAnchorResize(anchor as AnchorResize, event)
+              }
+            />
+          </E2EDataAttribute>
+        ))}
 
       {/* ROTATE AREA */}
-      {enumToArray(AnchorRotate).map((anchor) => (
-        <E2EDataAttribute
-          key={anchor as keyof typeof AnchorRotate}
-          type={E2EAttribute.anchorRotate}
-          value={kebabCase(anchor as string)}
-        >
-          <div
-            className={cx(
-              classNamesWithTheme.anchorRotate.name,
-              classNamesWithTheme.anchorRotate.modificators[
-                anchor as keyof typeof AnchorRotate
-              ],
-            )}
-            onMouseDown={(event) =>
-              onMouseDownAnchorRotate(anchor as AnchorRotate, event)
-            }
-          />
-        </E2EDataAttribute>
-      ))}
+      {enumToArray(AnchorRotate)
+        .filter((anchor) => anchor !== AnchorRotate.none)
+        .map((anchor) => (
+          <E2EDataAttribute
+            key={anchor as keyof typeof AnchorRotate}
+            type={E2EAttribute.anchorRotate}
+            value={kebabCase(anchor as string)}
+          >
+            <div
+              className={cx(
+                classNamesWithTheme.anchorRotate.name,
+                classNamesWithTheme.anchorRotate.modificators[
+                  anchor as keyof typeof AnchorRotate
+                ],
+              )}
+              onMouseDown={(event) =>
+                onMouseDownAnchorRotate(anchor as AnchorRotate, event)
+              }
+            />
+          </E2EDataAttribute>
+        ))}
     </div>
   );
 };
