6badfd972c4ae773aa08903d03298adb4abb034e
Eryk Sandecki <eryk.sandecki@gmail.com>
2025-09-25 15:59:20 +0200

    XD: icons

--- src/assets/svg.ts
+++ src/assets/svg.ts
@@ -4,7 +4,9 @@ import { ReactComponent as AlignHorizontalRight } from './svg/align-horizontal-r
 import { ReactComponent as AlignVerticalBottom } from './svg/align-vertical-bottom.svg';
 import { ReactComponent as AlignVerticalCenter } from './svg/align-vertical-center.svg';
 import { ReactComponent as AlignVerticalTop } from './svg/align-vertical-top.svg';
+import { ReactComponent as AutoHeight } from './svg/auto-height.svg';
 import { ReactComponent as AutoLayout } from './svg/auto-layout.svg';
+import { ReactComponent as AutoWidth } from './svg/auto-width.svg';
 import { ReactComponent as Check } from './svg/check.svg';
 import { ReactComponent as Close } from './svg/close.svg';
 import { ReactComponent as Comment } from './svg/comment.svg';
@@ -14,6 +16,8 @@ import { ReactComponent as EyesClosed } from './svg/eyes-closed.svg';
 import { ReactComponent as EyesDropper } from './svg/eyes-dropper.svg';
 import { ReactComponent as EyesOpened } from './svg/eyes-opened.svg';
 import { ReactComponent as FitLayout } from './svg/fit-layout.svg';
+import { ReactComponent as FixedHeight } from './svg/fixed-height.svg';
+import { ReactComponent as FixedWidth } from './svg/fixed-width.svg';
 import { ReactComponent as FlipHorizontal } from './svg/flip-horizontal.svg';
 import { ReactComponent as FlipVertical } from './svg/flip-vertical.svg';
 import { ReactComponent as FlowDefault } from './svg/flow-default.svg';
@@ -23,6 +27,10 @@ import { ReactComponent as FlowVertical } from './svg/flow-vertical.svg';
 import { ReactComponent as FrameTool } from './svg/frame-tool.svg';
 import { ReactComponent as HtmlTag } from './svg/html-tag.svg';
 import { ReactComponent as Logo } from './svg/logo.svg';
+import { ReactComponent as MaxHeight } from './svg/max-height.svg';
+import { ReactComponent as MaxWidth } from './svg/max-width.svg';
+import { ReactComponent as MinHeight } from './svg/min-height.svg';
+import { ReactComponent as MinWidth } from './svg/min-width.svg';
 import { ReactComponent as MoveHand } from './svg/move-hand.svg';
 import { ReactComponent as MoveTool } from './svg/move-tool.svg';
 import { ReactComponent as PlusOutlined } from './svg/plus-outlined.svg';
@@ -31,6 +39,7 @@ import { ReactComponent as Protractor } from './svg/protractor.svg';
 import { ReactComponent as Sample } from './svg/sample.svg';
 import { ReactComponent as StepBackwardOutlined } from './svg/step-backward-outlined.svg';
 import { ReactComponent as ToggleRotate } from './svg/toggle-rotate.svg';
+import { ReactComponent as Variant } from './svg/variant.svg';
 
 export const Icons = {
   AlignHorizontalCenter,
@@ -39,7 +48,9 @@ export const Icons = {
   AlignVerticalBottom,
   AlignVerticalCenter,
   AlignVerticalTop,
+  AutoHeight,
   AutoLayout,
+  AutoWidth,
   Check,
   Close,
   Comment,
@@ -49,6 +60,8 @@ export const Icons = {
   EyesDropper,
   EyesOpened,
   FitLayout,
+  FixedHeight,
+  FixedWidth,
   FlipHorizontal,
   FlipVertical,
   FlowDefault,
@@ -58,6 +71,10 @@ export const Icons = {
   FrameTool,
   HtmlTag,
   Logo,
+  MaxHeight,
+  MaxWidth,
+  MinHeight,
+  MinWidth,
   MoveHand,
   MoveTool,
   PlusOutlined,
@@ -66,4 +83,5 @@ export const Icons = {
   Sample,
   StepBackwardOutlined,
   ToggleRotate,
+  Variant,
 };
