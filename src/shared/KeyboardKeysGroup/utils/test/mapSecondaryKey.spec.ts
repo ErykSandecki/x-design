afa881e49f98eefe1e82cbf8cb558be1f9041c64
Eryk Sandecki <eryk.sandecki@gmail.com>
2025-10-10 20:21:12 +0200

    XD: coverage

--- src/shared/KeyboardKeysGroup/utils/test/mapSecondaryKey.spec.ts
+++ src/shared/KeyboardKeysGroup/utils/test/mapSecondaryKey.spec.ts
@@ -5,11 +5,19 @@ import { KeyboardKeys } from 'types';
 import { mapSecondaryKey } from '../mapSecondaryKey';
 
 describe('mapSecondaryKey', () => {
-  it('should return alt', () => {
+  it('should return key mapped', () => {
     // before
     const result = mapSecondaryKey(KeyboardKeys.a);
 
     // result
     expect(result).toBe('A');
+  });
+
+  it('should return key', () => {
+    // before
+    const result = mapSecondaryKey(KeyboardKeys['+']);
+
+    // result
+    expect(result).toBe(KeyboardKeys['+']);
   });
 });
