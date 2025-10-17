623e578ded94c800b7acb838cd85e510db7bc289
Eryk Sandecki <eryk.sandecki@gmail.com>
2025-10-17 13:18:44 +0200

    XD: refactor

--- cypress/support/UI/box.ts
+++ cypress/support/UI/box.ts
@@ -5,11 +5,11 @@ import { E2EAttribute } from 'types';
 import { getE2EAttribute } from '../../utils/getE2EAttribute';
 
 Cypress.Commands.add('box', (e2eValue: string) => {
-  const parts = e2eValue.split('.');
-  let chain = cy.get(getE2EAttribute(E2EAttribute.box, parts[0]));
+  const chains = e2eValue.split('.');
+  let chain = cy.get(getE2EAttribute(E2EAttribute.box, chains[0]));
 
-  for (let i = 1; i < parts.length; i++) {
-    chain = chain.get(getE2EAttribute(E2EAttribute.box, parts[i]));
+  for (let i = 1; i < chains.length; i++) {
+    chain = chain.get(getE2EAttribute(E2EAttribute.box, chains[i]));
   }
 
   return chain;
