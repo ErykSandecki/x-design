8b16420ed9d7d964cb85538bff818ab5a734b27c
Eryk Sandecki <eryk.sandecki@gmail.com>
2025-10-26 10:41:23 +0100

    XD: revert layout

--- src/store/pageBuilder/utils/changeParent/getPrevParentChildren.ts
+++ src/store/pageBuilder/utils/changeParent/getPrevParentChildren.ts
@@ -1,9 +1,11 @@
 // types
-import { TChildren } from 'types';
+import { TChildren, TElement } from 'types';
 import { TEvents } from '../../types';
+
+export const hasChildToExclude = (draggableElements: TEvents['draggableElements'], id: TElement['id']): boolean =>
+  draggableElements.some((draggableElement) => draggableElement.id === id);
 
 export const getPrevParentChildren = (
   draggableElements: TEvents['draggableElements'],
   prevParentChildren,
-): Array<TChildren> =>
-  prevParentChildren.filter(({ id }) => !draggableElements.some((draggableElement) => draggableElement.id === id));
+): Array<TChildren> => prevParentChildren.filter(({ id }) => !hasChildToExclude(draggableElements, id));
