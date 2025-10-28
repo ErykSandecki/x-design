adc321d683a31a25ce137d668e7290ab1cc3e447
Eryk Sandecki <eryk.sandecki@gmail.com>
2025-10-28 23:15:09 +0100

    XD: refactor

--- src/store/pageBuilder/utils/changeLayoutGrid/getCorrectGrid.ts
+++ src/store/pageBuilder/utils/changeLayoutGrid/getCorrectGrid.ts
@@ -1,5 +1,3 @@
-import { first } from 'lodash';
-
 // types
 import { ElementType, TElement, TGrid } from 'types';
 import { TChangeLayoutGridAction } from '../../types';
@@ -12,13 +10,11 @@ export const getTotalChildren = (children: TElement['children']): number => {
   return isOdd(totalElements) ? totalElements + 1 : totalElements;
 };
 
-export const getCorrectGrid = (cell: TChangeLayoutGridAction['payload'], element: TElement): TGrid => {
+export const getCorrectGrid = (cells: TChangeLayoutGridAction['payload'], element: TElement): TGrid => {
   const { children, layout } = element;
   const totalChildren = getTotalChildren(children);
-  const key1 = first(Object.keys(cell));
-  const grid = { ...layout.grid, [key1]: cell[key1] };
-  const { columns, rows } = grid;
-  const totalColumns = columns * rows;
+  const grid = { ...layout.grid, ...cells };
+  const totalColumns = grid.columns * grid.rows;
 
   return totalColumns < totalChildren ? gridSize(totalChildren, totalColumns) : grid;
 };
